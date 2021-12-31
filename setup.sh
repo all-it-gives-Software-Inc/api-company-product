22d4524877d8e98c69c334b5a7bbfa23127a0753
joao4018 <joao4018@gmail.com>
2021-12-31 10:58:29 -0300

    Added save a store

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-export DATABASE_URL = ''
+export DATABASE_URL = 'postgres://rcmbolvzkvcqbm:15353c503ed6bc46a081e2b3cad7c41b5087861b9bab7c899d7a91b9f27a95cf@ec2-52-70-205-234.compute-1.amazonaws.com:5432/d500cmlemclva1'
